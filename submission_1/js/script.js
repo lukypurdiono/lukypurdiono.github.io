20ef5ecbca8937dcaa64b0a561c310885f33956b
lukypurdiono <lukypurdiono@gmail.com>
2021-01-31 21:23:58 +0700

    eslint

--- submission_1/js/script.js
+++ submission_1/js/script.js
@@ -1,64 +1,60 @@
-document.addEventListener('DOMContentLoaded', function(){
+/* eslint-disable func-names */
+/* eslint-disable no-shadow */
+/* eslint-disable no-undef */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-param-reassign */
+/* eslint-disable eqeqeq */
+document.addEventListener('DOMContentLoaded', () => {
+  // SIDEBAR NAVIGATION
+  const elems = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(elems);
+  loadNav();
 
-	// SIDEBAR NAVIGATION
-	var elems = document.querySelectorAll('.sidenav');
-	M.Sidenav.init(elems);
-	loadNav();
+  function loadNav() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        if (this.status != 200) return;
 
-	function loadNav()
-	{
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4){
-				if(this.status != 200) return;
+        document.querySelectorAll('.topnav, .sidenav')
+          .forEach((elm) => {
+            elm.innerHTML = xhttp.responseText;
+          });
 
-				// Muat daftar tautan menu
-				document.querySelectorAll(".topnav, .sidenav, .card")
-				.forEach(function(elm){
-					elm.innerHTML = xhttp.responseText;
-				});
+        document.querySelectorAll('.sidenav a, .topnav a')
+          .forEach((elm) => {
+            elm.addEventListener('click', (event) => {
+              const sidenav = document.querySelector('.sidenav');
+              M.Sidenav.getInstance(sidenav).close();
+              page = event.target.getAttribute('href').substr(1);
+              loadPage(page);
+            });
+          });
+      }
+    };
+    xhttp.open('GET', 'nav.html', true);
+    xhttp.send();
+  }
 
-				// Daftarkan event listener untuk setiap tautan menu
-				document.querySelectorAll('.sidenav a, .topnav a, .card a')
-				.forEach(function(elm){
-					elm.addEventListener('click', function(event){
-						// Tutup sidenav
-						var sidenav = document.querySelector('.sidenav');
-						M.Sidenav.getInstance(sidenav).close();
-						
-						// Muat konten halaman yang dipanggil 
-						page = event.target.getAttribute('href').substr(1);
-						loadPage(page);
-					});
-				});
-			}
-		};
-		xhttp.open("GET", 'nav.html', true);
-		xhttp.send();
-	}
-	
-	// Load page content
-	var page = window.location.hash.substr(1);
-	if(page == '') page = 'home';
-	loadPage(page);
+  let page = window.location.hash.substr(1);
+  if (page == '') page = 'home';
+  loadPage(page);
 
-	function loadPage(page)
-	{
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4){
-				var content = document.querySelector(".body-content");
-				if(this.status == 200) {
-					content.innerHTML = xhttp.responseText;
-				} else if(this.status == 404) {
-					content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
-				} else {
-					content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
-				}
-			}
-		};
-		xhttp.open("GET", 'pages/'+page+'.html', true);
-		xhttp.send();
-	}
-
+  function loadPage(page) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        const content = document.querySelector('.body-content');
+        if (this.status == 200) {
+          content.innerHTML = xhttp.responseText;
+        } else if (this.status == 404) {
+          content.innerHTML = '<p>Halaman tidak ditemukan.</p>';
+        } else {
+          content.innerHTML = '<p>Ups.. halaman tidak dapat diakses.</p>';
+        }
+      }
+    };
+    xhttp.open('GET', `pages/${page}.html`, true);
+    xhttp.send();
+  }
 });
--- submission_1/service-worker.js
+++ submission_1/service-worker.js
@@ -1,61 +1,73 @@
-const CACHE_NAME = "submission1";
-var urlsToCache = [
-  "/",
-  "/nav.html",
-  "/index.html",
-  "/pages/home.html",
-  "/pages/about.html",
-  "/pages/contact.html",
-  "/pages/address.html",
-  "/pages/profile.html",
-  "/pages/sertificate.html",
-  "/pages/study.html",
-  "/css/materialize.min.css",
-  "/css/style.css",
-  "/js/materialize.min.js",
-  "/js/script.js",
-  "/img/about.svg",
-  "/img/bfwd.svg",
-  "/img/contact.svg",
-  "/img/map.svg",
-  "/img/medal.svg",
-  "/img/profile.png",
-  "/img/qrcode.svg",
-  "/img/school.svg",
-  "/img/user.svg",
-  "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;1,400&display=swap"
+/* eslint-disable consistent-return */
+const CACHE_NAME = 'submission1';
+const urlsToCache = [
+  '/',
+  '/nav.html',
+  '/index.html',
+  '/pages/home.html',
+  '/pages/about.html',
+  '/pages/contact.html',
+  '/pages/address.html',
+  '/pages/profile.html',
+  '/pages/sertificate.html',
+  '/pages/study.html',
+  '/css/materialize.min.css',
+  '/css/style.css',
+  '/js/materialize.min.js',
+  '/js/script.js',
+  '/img/about.svg',
+  '/img/bfwd.svg',
+  '/img/contact.svg',
+  '/img/map.svg',
+  '/img/medal.svg',
+  '/img/profile.png',
+  '/img/qrcode.svg',
+  '/img/school.svg',
+  '/img/user.svg',
+  'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;1,400&display=swap',
 ];
- 
-self.addEventListener("fetch", function(event) {
-    event.respondWith(
-      caches
-        .match(event.request, { cacheName: CACHE_NAME })
-        .then(function(response) {
-          if (response) {
-            console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
-            return response;
-          }
-   
-          console.log(
-            "ServiceWorker: Memuat aset dari server: ",
-            event.request.url
-          );
-          return fetch(event.request);
-        })
-    );
-  });
 
-  self.addEventListener("activate", function(event) {
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            if (cacheName != CACHE_NAME) {
-              console.log("ServiceWorker: cache " + cacheName + " dihapus");
-              return caches.delete(cacheName);
-            }
-          })
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
+  );
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request, { cacheName: CACHE_NAME })
+      .then((response) => {
+        if (response) {
+          // eslint-disable-next-line no-console
+          console.log('ServiceWorker: Gunakan aset dari cache: ', response.url);
+          return response;
+        }
+
+        // eslint-disable-next-line no-console
+        console.log(
+          'ServiceWorker: Memuat aset dari server: ',
+          event.request.url,
         );
-      })
-    );
-  });
+        return fetch(event.request);
+      }),
+  );
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      // eslint-disable-next-line array-callback-return
+      cacheNames.map((cacheName) => {
+        // eslint-disable-next-line eqeqeq
+        if (cacheName != CACHE_NAME) {
+          console.log(`ServiceWorker: cache ${cacheName} dihapus`);
+          return caches.delete(cacheName);
+        }
+      }),
+    )),
+  );
+});
